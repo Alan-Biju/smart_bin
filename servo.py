2646263ed97c98787a7a60c4ac39dba41db689ef
Alan-Biju <alanbiju238@gmail.com>
2022-05-14 15:02:00 +0530

    some changes

--- servo.py
+++ servo.py
@@ -11,19 +11,19 @@
 #---------------------------------------------------------------------------
 #---------------------------------------------------------------------------
 #--------------------------------------------------------------------------
-from gpiozero import AngularServo
-from time import sleep
-import RPi.GPIO as GPIO
-servo = AngularServo(17, min_pulse_width=0.0006, max_pulse_width=0.0023)
+# from gpiozero import AngularServo
+# from time import sleep
+# import RPi.GPIO as GPIO
+# servo = AngularServo(17, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
-def servoMotor():
-    servo.angle = 90
-    sleep(2)
-    servo.angle=-90
-    sleep(2)
-    servo.angle=0
+# def servoMotor():
+#     servo.angle = 90
+#     sleep(2)
+#     servo.angle=-90
+#     sleep(2)
+#     servo.angle=0
 
-    GPIO.cleanup()
+#     GPIO.cleanup()
    
     
 
